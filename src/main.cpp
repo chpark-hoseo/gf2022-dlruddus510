6b618660418e8992597fae1cdb5ab3b5d07f3996
20181214 이경연 <20181214@vision.hoseo.edu>
2022-09-14 01:05:32 +0900

    HelloSDL-과제
    1번. 30번 줄에서 rgb변경하여 배경색을 변경 했습니다.
    3번. 34번 줄에서 딜레이를 5000에서 10000으로 변경 했습니다.
    2번. 기존에 있던 윈도우 생성을 주석처리하고 새로운 윈도우 생성코드를 추가 하였습니다.

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,17 @@ int main(int argc, char* args[])
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
-		g_pWindow = SDL_CreateWindow("Game Framework",
+		/*g_pWindow = SDL_CreateWindow("Game Framework",
 			SDL_WINDOWPOS_CENTERED,
 			SDL_WINDOWPOS_CENTERED, 1024, 768,
-			SDL_WINDOW_SHOWN);
+			SDL_WINDOW_SHOWN);*/
+		
+		g_pWindow = SDL_CreateWindow("Test3",
+			SDL_WINDOWPOS_CENTERED,
+			SDL_WINDOWPOS_CENTERED,
+			300, 300,
+			SDL_WINDOW_BORDERLESS | SDL_WINDOW_MAXIMIZED);
+
 		if (g_pWindow != 0)
 		{
 			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
@@ -20,11 +27,11 @@ int main(int argc, char* args[])
 		return 1;
 	}
 
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 255, 255);
 	SDL_RenderClear(g_pRenderer);
 	SDL_RenderPresent(g_pRenderer);
 
-	SDL_Delay(5000);
+	SDL_Delay(10000);
 	SDL_Quit();
 
 	return 0;
