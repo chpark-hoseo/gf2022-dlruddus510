f330647f3c81e19e3c52e75ee6aa460e9fdb04a3
dlruddus510 <dlruddus510@gmail.com>
2022-09-30 13:23:47 +0900

    원본상자/대상상자-실습

--- include/Game.h
+++ include/Game.h
@@ -21,7 +21,6 @@ private:
 	SDL_Window* m_pWindow;
 	SDL_Renderer* m_pRenderer;
 	SDL_Texture* m_pTexture;
-	SDL_Renderer* renderer;
 
 	// 원본 사각형
 	SDL_Rect m_sourceRectangle;
@@ -29,18 +28,6 @@ private:
 	// 대상 사각형
 	SDL_Rect m_destinationRectangle;
 
-	SDL_Texture* m_pTexture2;
-
-	// 원본 사각형
-	SDL_Rect m_sourceRectangle2;
-
-	// 대상 사각형
-	SDL_Rect m_destinationRectangle2;
-
-
-	int SCREEN_WIDTH;
-	int SCREEN_HEIGHT;
-	int flag = 0;
 	bool m_bRunning;
 };
 
