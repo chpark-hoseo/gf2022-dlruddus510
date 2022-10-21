72708d7b66e3c6f2b79fe024f53fa61e0543efb4
20181214 이경연 <20181214@vision.hoseo.edu>
2022-10-21 11:45:10 +0900

    Game 1

--- include/Game.h
+++ include/Game.h
@@ -20,9 +20,24 @@ private:
 	SDL_Window* m_pWindow;
 	SDL_Renderer* m_pRenderer;
 
-	int time = 0;
 	int m_currentFrame;
 
+	int Ballturn = 0;
+	int Ballturn2 = 0;
+	int Ballturn3 = 0;
+	int Playerturn = 0;
+
+	int Ball_x = 0;
+	int Ball_y = 0;
+
+	int Ball2_x = 1000;
+	int Ball2_y = 0;
+
+	int Ball3_x = 0;
+	int Ball3_y = 100;
+
+	int Player_x = 200;
+	int Player_y = 390;
 
 	bool m_bRunning;
 };
--- src/Game.cpp
+++ src/Game.cpp
@@ -23,33 +23,101 @@ bool Game::init(const char* title, int xpos, int ypos, int height, int width, in
         return false; // SDL 초기화 실패
     }
 
-    if (!TheTextureManager::Instance()->load("Assets/animate-alpha.png", "animate", m_pRenderer))
+    if (!TheTextureManager::Instance()->load("assets/Background.png", "BG", m_pRenderer))
     {
         return false;
     }
-    
+
+    if (!TheTextureManager::Instance()->load("assets/Player.png", "Player", m_pRenderer))
+    {
+        return false;
+    }
+
+    if (!TheTextureManager::Instance()->load("assets/Ball.png", "Ball", m_pRenderer))
+    {
+        return false;
+    }
+
+    if (!TheTextureManager::Instance()->load("assets/Ball2.png", "Ball2", m_pRenderer))
+    {
+        return false;
+    }
+
+    if (!TheTextureManager::Instance()->load("assets/Ball3.png", "Ball3", m_pRenderer))
+    {
+        return false;
+    }
+
     m_bRunning = true;
     return true;
 }
 
 void  Game::update()
 {
-    time++;
-    m_currentFrame = ((SDL_GetTicks() / 100) % 6);
-    SDL_Delay(100);
+    if (Ballturn == 0)
+    {
+        Ball_y = Ball_y + 5;
+        Ball_x = Ball_x + 2;
+        if (Ball_y >= 320) Ballturn = 1;
+    }
+    if (Ballturn == 1)
+    {
+        Ball_y = Ball_y - 5;
+        Ball_x = Ball_x + 2;
+        if (Ball_y <= 0) Ballturn = 0;
+    }
+    if (Ball_x > 1000) Ball_x = -100;
+
+
+    if (Ballturn2 == 0)
+    {
+        Ball2_y = Ball2_y + 4;
+        Ball2_x = Ball2_x - 2;
+        if (Ball2_y >= 410) Ballturn2 = 1;
+    }
+    if (Ballturn2 == 1)
+    {
+        Ball2_y = Ball2_y - 4;
+        Ball2_x = Ball2_x - 2;
+        if (Ball2_y <= 200) Ballturn2 = 0;
+    }
+    if (Ball2_x < 0) Ball2_x = 1000;
+
+
+    if (Ballturn3 == 0)
+    {
+        Ball3_y = Ball3_y + 3;
+        Ball3_x = Ball3_x + 1;
+        if (Ball3_y >= 400) Ballturn3 = 1;
+    }
+    if (Ballturn3 == 1)
+    {
+        Ball3_y = Ball3_y - 3;
+        Ball3_x = Ball3_x + 1;
+        if (Ball3_y <= -100) Ballturn3 = 0;
+    }
+    if (Ball3_x > 1000) Ball3_x = 0;
+
+    
+    m_currentFrame = ((SDL_GetTicks() / 100) % 8);
+    SDL_Delay(10);
 }
 
 void Game::render()
 {     
     SDL_RenderClear(m_pRenderer); 
-    TheTextureManager::Instance()->draw("animate", 0, 0, 128, 82,
+
+    TheTextureManager::Instance()->draw("BG", 0, 0, 1000, 640,
         m_pRenderer);
-    if (time >= 100) // 10초 뒤에 움직이지 않는 텍스쳐 삭제
-    {
-        SDL_RenderClear(m_pRenderer);
-    }
-    TheTextureManager::Instance()->drawFrame("animate", 100, 100, 128,
-        82, 0, m_currentFrame, m_pRenderer);
+    TheTextureManager::Instance()->drawFrame("Player", Player_x, Player_y, 128,
+        128, 0, m_currentFrame, m_pRenderer);
+    TheTextureManager::Instance()->draw("Ball", Ball_x, Ball_y, 180, 180,
+        m_pRenderer);
+    TheTextureManager::Instance()->draw("Ball2", Ball2_x, Ball2_y, 90, 90,
+        m_pRenderer);
+    TheTextureManager::Instance()->draw("Ball3", Ball3_x, Ball3_y, 100, 100,
+        m_pRenderer);
+
     SDL_RenderPresent(m_pRenderer);
 }
 
